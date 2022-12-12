c524090ee3fc73ff008a5006d83812bee5c6acfb
Connor Bratten <connorabratten@gmail.com>
2022-12-12 11:27:29 -0500

    Add part 2 for day 1

--- day_01/cal_count.py
+++ day_01/cal_count.py
@@ -17,3 +17,7 @@ for line in lines:
 
 max_elf = sorted(elves.items(), key=lambda elf: elf[1])[-1][1]
 print(f'The elf carrying the most calories is carrying {max_elf} calories.')
+
+
+max_three_elves = sum([elf[1] for elf in sorted(elves.items(), key=lambda elf: elf[1])[-3:]])
+print(f'The three elves carrying the most calories are carrying {max_three_elves} calories.')
